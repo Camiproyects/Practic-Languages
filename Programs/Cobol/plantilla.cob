d023f15b72cf8018b67be1589c7a7691d88e86b7
CamiProyects <andresillopillo2004@gmail.com>
2025-01-29 16:44:55 -0500

    29-01-2025.04

--- Programs/Cobol/plantilla.cob
+++ Programs/Cobol/plantilla.cob
@@ -11,10 +11,14 @@
              03 VARIABLES.
                  05 COD          PIC 9(6).
                  05 DES          PIC X(50).
-                 05 PRECOM       PIC 9(8)V99.
-                 05 PREVEN       PIC 9(8)V99.
-                 05 INV          PIC 9(7)V99.
+                 05 PRECOM       PIC 9(8)V9(2).
+                 05 PREVEN       PIC 9(8)V9(2).
+                 05 INV          PIC 9(7)V9(2).
                  05 DAT          PIC X.
+                 05 COD-EDIT     PIC   ZZZZ,ZZ9.
+                 05 PRECOM-EDIT  PIC ZZZ,ZZZ,ZZ9.99.
+                 05 PREVEN-EDIT  PIC ZZZ,ZZZ,ZZ9.99.
+                 05 INV-EDIT    PIC  ZZ,ZZZ,ZZ9.99.
                  
         PROCEDURE DIVISION.
         INICIO.
@@ -30,16 +34,31 @@
               DISPLAY '3- PRECIO COMPRA :' LINE 9  POSITION 20.
               DISPLAY '4- PRECIO VENTA  :' LINE 11 POSITION 20.
               DISPLAY '5- INVENTARIO    :' LINE 13 POSITION 20.
-              DISPLAY '6- DATOS         :' LINE 21 POSITION 20.
-              PERFORM TOMA-DATOS.
+              DISPLAY '6- DATOS(S/NUM)  :' LINE 21 POSITION 20.
+              PERFORM TOMA-DATOS.	
 
         TOMA-DATOS.
-              ACCEPT COD                   LINE 5  POSITION 40.
-              ACCEPT DES                   LINE 7  POSITION 40.
-              ACCEPT PRECOM                LINE 9  POSITION 40.
-              ACCEPT PREVEN                LINE 11 POSITION 40.
-              ACCEPT INV                   LINE 13 POSITION 40.
-              ACCEPT DAT                   LINE 21 POSITION 40.
+              ACCEPT  COD                  LINE 5  POSITION 46 REQUIRED.
+               MOVE    COD TO COD-EDIT.
+               DISPLAY COD-EDIT            LINE 5  POSITION 46.
+               
+              ACCEPT  DES                  LINE 7  POSITION 40 REQUIRED.
+              
+              ACCEPT  PRECOM               LINE 9  POSITION 40 REQUIRED.
+               MOVE    PRECOM TO PRECOM-EDIT.
+               DISPLAY PRECOM-EDIT         LINE 9  POSITION 40.
+               
+              ACCEPT  PREVEN               LINE 11 POSITION 40 REQUIRED.
+               MOVE    PREVEN TO PREVEN-EDIT.
+               DISPLAY PREVEN-EDIT         LINE 11 POSITION 40.
+               
+              ACCEPT  INV                  LINE 13 POSITION 41 REQUIRED.
+               MOVE    INV TO INV-EDIT.
+               DISPLAY INV-EDIT            LINE 13 POSITION 41.
+               
+              ACCEPT DAT                   LINE 21 POSITION 40	.
+              
+              
               IF DAT = 'S' OR DAT ='s'
                 PERFORM PANTALLA
               ELSE
@@ -49,20 +68,28 @@
         UNOAUNO.
                 EVALUATE DAT
                   WHEN 1
-                   ACCEPT COD              LINE 5  POSITION 40
+                   ACCEPT COD              LINE 5  POSITION 46
                    MOVE 0 TO DAT
+                   MOVE    COD TO COD-EDIT
+                   DISPLAY COD-EDIT            LINE 5  POSITION 46
                   WHEN 2
                    ACCEPT DES              LINE 7  POSITION 40
                    MOVE 0 TO DAT
                   WHEN 3
                    ACCEPT PRECOM           LINE 9  POSITION 40
                    MOVE 0 TO DAT
+                   MOVE    PRECOM TO PRECOM-EDIT
+                   DISPLAY PRECOM-EDIT         LINE 9  POSITION 40
                   WHEN 4
                    ACCEPT PREVEN           LINE 11 POSITION 40
                    MOVE 0 TO DAT
+                   MOVE    PREVEN TO PREVEN-EDIT
+                   DISPLAY PREVEN-EDIT         LINE 11 POSITION 40
                   WHEN 5
-                   ACCEPT INV              LINE 13 POSITION 40
+                   ACCEPT INV              LINE 13 POSITION 41
                    MOVE 0 TO DAT
+                   MOVE    INV TO INV-EDIT
+                   DISPLAY INV-EDIT            LINE 13 POSITION 41
                   WHEN 0
                    ACCEPT DAT              LINE 21 POSITION 40
                   WHEN 'S'
