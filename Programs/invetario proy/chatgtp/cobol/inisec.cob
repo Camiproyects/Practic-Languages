453ba0ace88c3a76a58e25e3245dbfc233ff7dcd
Camiproyects <andresillopillo2004@gmail.com>
2025-02-03 13:55:40 +0000

    03-02-2025.02

--- Programs/invetario proy/chatgtp/cobol/inisec.cob
+++ Programs/invetario proy/chatgtp/cobol/inisec.cob
@@ -1,32 +1,36 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. inisec.
-        
-       ENVIROMENT DIVISION.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT USUARIOS ASSIGN TO "data/usuarios.json"
            ORGANIZATION IS LINE SEQUENTIAL.
-        
+
        DATA DIVISION.
+       FILE SECTION.
+       FD  USUARIOS.
+       01  USUARIO-REGISTRO PIC X(100).
+
        WORKING-STORAGE SECTION.
-        
-    	01 DATUSU.
-    	   03 PERSIS.
-	         05 NUMDOC      PIC 9(10).
-	         05 CAR         PIC X(05).
-	         05 CODPASS     PIC 9(04).
+       01 DATUSU.
+           03 PERSIS.
+               05 NUMDOC      PIC 9(10).
+               05 CAR         PIC X(05).
+               05 CODPASS     PIC 9(04).
            03 INFADD.
-             05 NOM         PIC X(50).
-             05 TIPDOC      PIC XXX.
-             05 COR         PIC X(20).
-             05 CANPAG      PIC 9(10).
-             05 DET         PIC X(50).
+               05 NOM         PIC X(50).
+               05 TIPDOC      PIC XXX.
+               05 COR         PIC X(20).
+               05 CANPAG      PIC 9(10).
+               05 DET         PIC X(50).
            03 PANTALLA.
-             05 DAT         PIC XX.
-             05 MARFILL     PIC X(50) VALUE ALL '*'.
-             05 MAR         PIC X.
-             05 SW          PIC X.
-       
+               05 DAT         PIC XX.
+               05 MARFILL     PIC X(50) VALUE ALL '*'.
+               05 MAR         PIC X.
+               05 SW          PIC X.
+
        PROCEDURE DIVISION.
        INICIO.
            MOVE ' ' TO DAT.
@@ -37,10 +41,14 @@
            DISPLAY '*NUMERO DE DOCUMENTO   :'     LINE 5  POSITION 20.
            DISPLAY '*NUMERO DE EMPLEADO    :'     LINE 7  POSITION 20.
            PERFORM ACPDAT UNTIL SW = 'Q'.
-           
+
            STOP RUN.
+
        ACPDAT.
-           ACCEPT NUMDOC                          LINE 5  POSIITON 40.
-           ACCEPT CODPASS                         LINE 5  POSIITON 40.
-           OPEN I-O USUARIOS USUARIOS.
-           
+           ACCEPT NUMDOC                          LINE 5  POSITION 40.
+           ACCEPT CODPASS                         LINE 7  POSITION 40.
+           OPEN INPUT USUARIOS.
+           READ USUARIOS INTO USUARIO-REGISTRO
+               AT END MOVE 'Q' TO SW
+               NOT AT END DISPLAY USUARIO-REGISTRO.
+           CLOSE USUARIOS.
