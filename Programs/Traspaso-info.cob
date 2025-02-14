4e151f65a9be5f635674a186131e6e1d3168c1ea
CamiProyects <andresillopillo2004@gmail.com>
2025-02-14 15:45:34 -0500

    14-02-2025

--- Programs/Traspaso-info.cob
+++ Programs/Traspaso-info.cob
@@ -18,7 +18,7 @@
        DATA DIVISION.
        FILE SECTION.
        FD ARCHIVO-BASE.
-       01 REGISTRO PIC X(291).
+       01 REGISTRO       PIC X(291).
 
        FD ARCHIVO-SALIDA.
        01 USUDATA.
@@ -34,74 +34,261 @@
        WORKING-STORAGE SECTION.
        01 WS-DATA.
            05 WS-FS          PIC XX.
+           05 WS-PER         PIC XX.
            05 WS-FS-SALIDA   PIC XX.
            05 WS-OPTI        PIC X     VALUE SPACE.
-           05 CONTADOR       PIC 9(10) VALUE 0.
            05 LIN            PIC 9(5)  VALUE 0.
+           05 CONTADOR       PIC 9(10) VALUE 0.
+           05 ERROR-COUNT    PIC 9(10) VALUE 0.
+           05 T-CONTADOR     PIC X(10).
+           05 T-ERROR-COUNT  PIC X(10).
 
-       01 HEADER        PIC X(291) VALUE SPACES.
+       01 WS-MSG           PIC X(80) VALUE SPACES.
+       01 HEADER           PIC X(291) VALUE SPACES.
 
        PROCEDURE DIVISION.
        INICIO.
            MOVE 0 TO CONTADOR.
+           MOVE "Y" TO WS-PER.
            OPEN INPUT ARCHIVO-BASE.
            IF WS-FS NOT = "00"
-               DISPLAY "ERROR ABRIENDO ARCHIVO BASE: " WS-FS
-               STOP RUN
+             STRING "ERROR ABRIENDO ARCHIVO BASE: " DELIMITED BY SIZE
+               WS-FS DELIMITED BY SIZE
+               INTO WS-MSG
+             DISPLAY WS-MSG
+             STOP RUN
            END-IF.
+           
+           PERFORM UNTIL WS-PER = "Q"
+             IF WS-PER = "Y"
+               PERFORM ABRIR
+             ELSE
+               PERFORM LEER
+             END-IF
+           END-PERFORM.
+           
+       ABRIR.
+           OPEN OUTPUT ARCHIVO-SALIDA.
+           IF WS-FS-SALIDA NOT = "00"
+             STRING "Error al abrir el archivo " DELIMITED BY SIZE
+               INTO WS-MSG
+               DISPLAY WS-MSG
+             STOP RUN
+           END-IF.
+       
+           MOVE "NUMDOC" TO NUMDOC.
+           MOVE "RAZSOC" TO RAZSOC.
+           MOVE "DIRECT" TO DIRECT.
+           MOVE "CIUDAD" TO CIUDAD.
+           MOVE "CONTAC" TO CONTAC.
+           MOVE "TEL-1" TO TEL-1.
+           MOVE "TEL-2" TO TEL-2.
+           MOVE "CORREO" TO CORREO.
 
+           WRITE USUDATA.
+           IF WS-FS-SALIDA NOT = "00"
+             STRING "Error al escribir en el archivo." DELIMITED BY SIZE
+               INTO WS-MSG
+             DISPLAY WS-MSG
+           END-IF.
+           MOVE "E" TO WS-PER.
+           CLOSE ARCHIVO-SALIDA.
+           
+           
+       LEER.
            OPEN I-O ARCHIVO-SALIDA.
            IF WS-FS-SALIDA NOT = "00" AND WS-FS-SALIDA NOT = "05"
-               DISPLAY "ERROR ABRIENDO ARCHIVO SALIDA: " WS-FS-SALIDA
-               CLOSE ARCHIVO-BASE
-               STOP RUN
+             STRING "ERROR ABRIENDO ARCHIVO SALIDA: " DELIMITED BY SIZE
+               WS-FS-SALIDA DELIMITED BY SIZE
+               INTO WS-MSG
+             DISPLAY WS-MSG
+             CLOSE ARCHIVO-BASE
+             STOP RUN
            END-IF.
-
+       
            READ ARCHIVO-BASE INTO HEADER
-               AT END 
-                   DISPLAY "Archivo vacío o sin encabezado."
-                   CLOSE ARCHIVO-BASE
-                   CLOSE ARCHIVO-SALIDA
-                   STOP RUN.
-
-           DISPLAY "PROCESANDO DATOS..." LINE 2 POSITION 10.
-
+             AT END 
+              STRING "Archivo vacío ." DELIMITED BY SIZE
+              INTO WS-MSG
+              DISPLAY WS-MSG
+              CLOSE ARCHIVO-BASE
+              CLOSE ARCHIVO-SALIDA
+              STOP RUN
+           END-READ.
+ 
+           STRING "PROCESANDO DATOS..." DELIMITED BY SIZE
+                  INTO WS-MSG
+           DISPLAY WS-MSG LINE 2 POSITION 10.
+           
+           STRING "REGISTROS CON ERROR:" DELIMITED BY SIZE
+                  INTO WS-MSG
+           DISPLAY WS-MSG LINE 3 POSITION 10.
+           
            MOVE 10 TO LIN.
 
            PERFORM UNTIL WS-OPTI = "Q"
-               READ ARCHIVO-BASE INTO REGISTRO
-                   AT END MOVE "Q" TO WS-OPTI
-                   NOT AT END 
-                   ADD 1 TO CONTADOR
-                   PERFORM PROCESAR-REGISTRO
-                   PERFORM VER
-               END-READ
+             READ ARCHIVO-BASE INTO REGISTRO
+               AT END MOVE "Q" TO WS-OPTI
+               NOT AT END 
+                 ADD 1 TO CONTADOR
+                 PERFORM PROCESAR-REGISTRO
+                 PERFORM VER
+             END-READ
            END-PERFORM.
-
+           
            CLOSE ARCHIVO-BASE.
            CLOSE ARCHIVO-SALIDA.
            STOP RUN.
-       
+
        PROCESAR-REGISTRO.
            UNSTRING REGISTRO DELIMITED BY ';'
-               INTO NUMDOC, RAZSOC, DIRECT, 
-               CIUDAD, CONTAC, TEL-1, TEL-2, CORREO.
-           
-           WRITE USUDATA.
-           
+             INTO NUMDOC, RAZSOC, DIRECT, 
+                    CIUDAD, CONTAC, TEL-1, TEL-2, CORREO.
+           IF NUMDOC = ZEROS OR RAZSOC = SPACES OR DIRECT = SPACES OR 
+              CIUDAD = SPACES OR TEL-1 = SPACES OR CORREO = SPACES
+              ADD 1 TO ERROR-COUNT
+           ELSE
+              WRITE USUDATA
+           END-IF.
+           EXIT.
+
        VER.
            ADD 1 TO LIN.
-           DISPLAY CONTADOR LINE 2 POSITION 50.
-           DISPLAY "*"     LINE LIN POSITION 1.
-           DISPLAY "*"     LINE LIN POSITION 12.
-           DISPLAY "*"     LINE LIN POSITION 34.
-           DISPLAY "*"     LINE LIN POSITION 45.
-           DISPLAY "*"     LINE LIN POSITION 56.
-           DISPLAY NUMDOC  LINE LIN POSITION 2.
-           DISPLAY RAZSOC  LINE LIN POSITION 13.
-           DISPLAY CIUDAD  LINE LIN POSITION 35.
-           DISPLAY CONTAC  LINE LIN POSITION 46.
-           DISPLAY TEL-1   LINE LIN POSITION 57.
+           
+           MOVE CONTADOR TO  T-CONTADOR.
+           MOVE ERROR-COUNT TO  T-ERROR-COUNT.
+           DISPLAY T-CONTADOR         LINE 2 POSITION 50.
+           DISPLAY T-ERROR-COUNT      LINE 3 POSITION 50.
+           DISPLAY "*"              LINE LIN POSITION 1.
+           DISPLAY "*"              LINE LIN POSITION 14.
+           DISPLAY "*"              LINE LIN POSITION 30.
+           DISPLAY "*"              LINE LIN POSITION 40.
+           DISPLAY "*"              LINE LIN POSITION 50.
+           DISPLAY NUMDOC           LINE LIN POSITION 2.
+           DISPLAY RAZSOC           LINE LIN POSITION 15.
+           DISPLAY CIUDAD           LINE LIN POSITION 31.
+           DISPLAY CONTAC           LINE LIN POSITION 41.
+           DISPLAY TEL-1            LINE LIN POSITION 51.
            IF LIN > 20 
               MOVE 10 TO LIN
            END-IF.
+           EXIT.
+
+      *     IF NUMDOC = ZEROES
+      *         DISPLAY "ERROR: No contiene número de cédula." 
+      *         MOVE "N" TO WS-REGVALID
+      *     END-IF.
+      *     IF RAZSOC = SPACES
+      *         DISPLAY "ERROR: No contiene razón social."
+      *         MOVE "N" TO WS-REGVALID
+      *     END-IF.
+      *     IF DIRECT = SPACES
+      *         DISPLAY "ERROR: No contiene dirección."
+      *         MOVE "N" TO WS-REGVALID
+      *     END-IF.
+      *     IF CIUDAD = SPACES
+      *         DISPLAY "ERROR: No contiene ciudad."
+      *         MOVE "N" TO WS-REGVALID
+      *     END-IF.
+      *     IF TEL-1 = SPACES
+      *         DISPLAY "ERROR: No contiene teléfono #1."
+      *         MOVE "N" TO WS-REGVALID
+      *     END-IF.
+      *     IF CORREO = SPACES
+      *         DISPLAY "ERROR: No contiene correo."
+      *         MOVE "N" TO WS-REGVALID
+      *     END-IF.*
+
+      *     IF WS-REGVALID = "N"
+      *        DISPLAY "El registro presenta errores. ¿Desea corregirlo? (S/N):" 
+      *               LINE 10 POSITION 10.
+      *        ACCEPT WS-RESPUESTA LINE 10 POSITION 50.
+      *        IF WS-RESPUESTA = "S" OR WS-RESPUESTA = "s"
+      *            PERFORM CORREGIRAR-REGISTRO
+      *            * Revalidar registro corregido
+      *            MOVE "Y" TO WS-REGVALID.
+      *            IF NUMDOC = ZEROES
+      *                DISPLAY "ERROR: No contiene número de cédula."
+      *                MOVE "N" TO WS-REGVALID
+      *            END-IF.
+      *            IF RAZSOC = SPACES
+      *                DISPLAY "ERROR: No contiene razón social."
+      *                MOVE "N" TO WS-REGVALID
+      *            END-IF.
+      *            IF DIRECT = SPACES
+      *                DISPLAY "ERROR: No contiene dirección."
+      *                MOVE "N" TO WS-REGVALID
+      *            END-IF.
+      *            IF CIUDAD = SPACES
+      *                DISPLAY "ERROR: No contiene ciudad."
+      *                MOVE "N" TO WS-REGVALID
+      *            END-IF.
+      *            IF TEL-1 = SPACES
+      *                DISPLAY "ERROR: No contiene teléfono #1."
+      *                MOVE "N" TO WS-REGVALID
+      *            END-IF.
+      *            IF CORREO = SPACES
+      *                DISPLAY "ERROR: No contiene correo."
+      *                MOVE "N" TO WS-REGVALID
+      *            END-IF.
+      *            IF WS-REGVALID = "Y"
+      *                WRITE USUDATA
+      *            ELSE
+      *                ADD 1 TO ERROR-COUNT
+      *                DISPLAY "El registro sigue presentando errores. Registro omitido." 
+      *                    LINE 12 POSITION 10.
+      *            END-IF.
+      *        ELSE
+      *            ADD 1 TO ERROR-COUNT
+      *            DISPLAY "Registro omitido." LINE 12 POSITION 10.
+      *        END-IF.
+      *     ELSE
+      *        WRITE USUDATA
+      *     END-IF.
+      *     EXIT.*
+
+      * CORREGIRAR-REGISTRO.
+      *     IF NUMDOC = ZEROES
+      *         DISPLAY "Ingrese número de cédula:" LINE 15 POSITION 10.
+      *         ACCEPT NUMDOC LINE 15 POSITION 35.
+      *     END-IF.
+      *     IF RAZSOC = SPACES
+      *         DISPLAY "Ingrese razón social:" LINE 16 POSITION 10.
+      *         ACCEPT RAZSOC LINE 16 POSITION 35.
+      *     END-IF.
+      *     IF DIRECT = SPACES
+      *         DISPLAY "Ingrese dirección:" LINE 17 POSITION 10.
+      *         ACCEPT DIRECT LINE 17 POSITION 35.
+      *     END-IF.
+      *     IF CIUDAD = SPACES
+      *         DISPLAY "Ingrese ciudad:" LINE 18 POSITION 10.
+      *         ACCEPT CIUDAD LINE 18 POSITION 35.
+      *     END-IF.
+      *     IF TEL-1 = SPACES
+      *         DISPLAY "Ingrese teléfono #1:" LINE 19 POSITION 10.
+      *         ACCEPT TEL-1 LINE 19 POSITION 35.
+      *     END-IF.
+      *     IF CORREO = SPACES
+      *         DISPLAY "Ingrese correo:" LINE 20 POSITION 10.
+      *         ACCEPT CORREO LINE 20 POSITION 35.
+      *     END-IF.
+      *     EXIT.*
+
+      * VER.
+      *     ADD 1 TO LIN.
+      *     DISPLAY CONTADOR LINE 2 POSITION 50.
+      *     DISPLAY "*"     LINE LIN POSITION 1.
+      *     DISPLAY "*"     LINE LIN POSITION 12.
+      *     DISPLAY "*"     LINE LIN POSITION 34.
+      *     DISPLAY "*"     LINE LIN POSITION 45.
+      *     DISPLAY "*"     LINE LIN POSITION 56.
+      *     DISPLAY NUMDOC  LINE LIN POSITION 2.
+      *     DISPLAY RAZSOC  LINE LIN POSITION 13.
+      *     DISPLAY CIUDAD  LINE LIN POSITION 35.
+      *     DISPLAY CONTAC  LINE LIN POSITION 46.
+      *     DISPLAY TEL-1   LINE LIN POSITION 57.
+      *     IF LIN > 20 
+      *        MOVE 10 TO LIN
+      *     END-IF.
+      *     EXIT.
+
